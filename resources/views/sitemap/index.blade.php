e9b19c0d1abe037b84c0061fdaff3b5d58caeef8
Nishant <nishantkoirala16@gmail.com>
2026-08-18 11:13:03 +0545

    Add Sitemap and SEO features

---
+++ resources/views/sitemap/index.blade.php
@@ -0,0 +1,23 @@
+{!! '<' . '?xml version="1.0" encoding="UTF-8"?>' !!}
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>{{ url('/') }}</loc>
+        <lastmod>{{ now()->tz('UTC')->toAtomString() }}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>{{ url('/products') }}</loc>
+        <lastmod>{{ now()->tz('UTC')->toAtomString() }}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.9</priority>
+    </url>
+    @foreach ($products as $product)
+        <url>
+            <loc>{{ url('/products/' . $product->slug) }}</loc>
+            <lastmod>{{ $product->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+        </url>
+    @endforeach
+</urlset>
